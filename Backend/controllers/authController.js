5af1f2cace86ace6c2f61866fff5336672ef7b83
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-02-02 18:23:30 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -26,8 +26,8 @@ exports.signup = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
