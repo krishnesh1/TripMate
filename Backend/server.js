1104fd76f7b21e3057b55d5b3715c35f2dfdaf15
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-02-07 15:21:37 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/members', require('./routes/memberRoutes'));
 app.use('/api/expenses', require('./routes/expenseRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
