56513c928d214773f72410800b110a0e9aef23f2
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-02-07 15:58:38 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  preview:{
+    host:true,
+    port:8000,
+    allowedHosts:['normal-kite-krishnesh-6e63647c.koyeb.app']
+  }
 })
