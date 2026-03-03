8d64514ab8b059f1fa7efae1274ced35240a26be
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-03-03 11:21:50 +0530

    Change SameSite cookie attribute to 'none'

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -27,7 +27,7 @@ exports.signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
