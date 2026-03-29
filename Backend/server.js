10517270555fd8e89d5c1374b8e921158b6921aa
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-03-29 23:11:57 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const cookieParser = require("cookie-parser")
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
